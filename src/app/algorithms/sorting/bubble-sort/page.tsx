8ffdb696911aa7b45f9e664368edec05684dd63d
odczik <58082541+odczik@users.noreply.github.com>
2025-06-23 11:27:33 +0200

    Add Bubble Sort playground UI with controls
    Introduces a new interactive UI for the Bubble Sort page, including a canvas for visualization and play/pause/forward controls. Updates global and module CSS to style the playground and its controls, improving layout and responsiveness.

--- src/app/algorithms/sorting/bubble-sort/page.tsx
+++ src/app/algorithms/sorting/bubble-sort/page.tsx
@@ -1,5 +1,35 @@
+'use client'
+
+import { useRef, useState } from "react";
+
 export default function BubbleSort() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    const [play, setPlay] = useState(false);
+
+    const handlePlay = () => {
+        setPlay(!play);
+    }
+
     return (
-        <h1>Bubble sort page</h1>
+        <div className="algorithmPlayground">
+            <h1>Bubble sort page</h1>
+            <canvas ref={canvasRef} width={1280} height={960} />
+            <div className="controlsContainer">
+                <div className="generalControls">
+                    <button className="iconContainer" onClick={handlePlay}>
+                        {play ? (
+                            <span className="icon" style={{maskImage: "url('https://img.icons8.com/material-rounded/96/play--v1.png')"}}></span>
+                        ) : (
+                            <span className="icon" style={{maskImage: "url('https://img.icons8.com/material-rounded/96/pause.png')"}}></span>
+                        )}
+                    </button>
+                    <button className="iconContainer"><span className="icon" style={{maskImage: "url('https://img.icons8.com/material-rounded/96/redo.png')"}}></span></button>
+                </div>
+                <div className="algorithmControls">
+
+                </div>
+            </div>
+        </div>
     );
 }
